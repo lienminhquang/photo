ed0e1088268fe6e8a4074d10c7b629a92bcf6683
lmq <minhquang130698@gmail.com>
2021-09-21 15:04:04 +0700

    + gen equality for userregister state

--- lib/bloc/userregister_state.dart
+++ lib/bloc/userregister_state.dart
@@ -11,4 +11,14 @@ class UserRegisterFailed extends UserregisterState {
   final UserRegiserFailedResult result;
 
   UserRegisterFailed(this.result);
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is UserRegisterFailed && other.result == result;
+  }
+
+  @override
+  int get hashCode => result.hashCode;
 }
